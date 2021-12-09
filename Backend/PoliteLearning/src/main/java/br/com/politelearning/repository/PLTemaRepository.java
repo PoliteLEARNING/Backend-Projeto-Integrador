b63ee75b270d407f16f1456b494f4b8eb8e93199
Gabriel Duarte <gabrielduarte_jardim@hotmail.com>
2021-12-09 11:09:18 -0300

    Adicionando Repositoy

---
+++ Backend/PoliteLearning/src/main/java/br/com/politelearning/repository/PLTemaRepository.java
@@ -0,0 +1,13 @@
+package br.com.politelearning.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.politelearning.model.PLTemaModel;
+
+public interface PLTemaRepository extends JpaRepository<PLTemaModel, Long> {
+	
+	public List<PLTemaModel> findAllbyPLTemaModelContainingIgnoreCase(String PLTemaModel);
+
+}
