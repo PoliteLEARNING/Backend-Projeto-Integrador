92b1ac79978d79a452e59914d171b2dcffd7f4c1
Gabriel Costa <gacosil0204@gmail.com>
2021-12-10 11:08:35 -0300

    Adicionando Model e Repository da tabela Postagem

---
+++ Backend/PoliteLearning/src/main/java/br/com/politelearning/model/PLPostagemModel.java
@@ -0,0 +1,92 @@
+package br.com.politelearning.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_postagem")
+public class PLPostagemModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank(message = "O atributo título é obrigatório!")
+	@Size(min = 3, max = 70, message = "O atributo título deve conter no mínimo 3 caracteres e no máximo 50.")
+	private String titulo;
+	
+	@NotBlank(message = "O atributo conteúdo é obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo conteúdo deve conter no mínimo 10 caracteres e no máximo 255.")
+	private String conteudo;
+	
+	private String imagem; // Conteúdo de Imagem é opcional, o usuário não precisa necessariamente colocar algo nesta variável.
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private PLTemaModel tema;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public PLTemaModel getTema() {
+		return tema;
+	}
+
+	public void setTema(PLTemaModel tema) {
+		this.tema = tema;
+	}
+	
+	
+}
