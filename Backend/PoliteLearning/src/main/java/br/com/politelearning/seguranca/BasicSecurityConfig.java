63f000ac38733ee2eae57a90c17feb2dcd01181a
gersonespejo127@gmail.com <gersonespejo127@gmail.com>
2022-01-26 17:48:09 -0300

    Atualizando serviceconfig

--- Backend/PoliteLearning/src/main/java/br/com/politelearning/seguranca/BasicSecurityConfig.java
+++ Backend/PoliteLearning/src/main/java/br/com/politelearning/seguranca/BasicSecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.politelearning.seguranca;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +11,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+
 
 @EnableWebSecurity
 
@@ -23,6 +26,12 @@ WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth)
 	throws Exception {
 	auth.userDetailsService(userDetailsService);
+	
+	auth.inMemoryAuthentication()
+	.withUser("root")
+	.password(passwordEncoder().encode("root"))
+	.authorities("ROLE_USER");
+
 	}
 
 	@Bean
@@ -35,6 +44,7 @@ WebSecurityConfigurerAdapter {
 	http.authorizeRequests()
 	.antMatchers("/usuarios/logar").permitAll()
 	.antMatchers("/usuarios/cadastrar").permitAll()
+	.antMatchers(HttpMethod.OPTIONS).permitAll()
 	.anyRequest().authenticated()
 	.and().httpBasic()
 	.and().sessionManagement().sessionCreationPolicy
