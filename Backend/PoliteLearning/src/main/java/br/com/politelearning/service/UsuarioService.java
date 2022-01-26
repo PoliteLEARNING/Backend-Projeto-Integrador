3d89151061a6ae19180b3401bcb25d23ed6251d8
moreiralaRI00 <93771450+moreiralaRI00@users.noreply.github.com>
2022-01-26 11:14:53 -0300

    integração com o front-end

--- Backend/PoliteLearning/src/main/java/br/com/politelearning/service/UsuarioService.java
+++ Backend/PoliteLearning/src/main/java/br/com/politelearning/service/UsuarioService.java
@@ -60,9 +60,9 @@ public class UsuarioService {
 
 				usuarioLogin.get().setId(usuario.get().getId());				
 				usuarioLogin.get().setNome(usuario.get().getNome());
-				usuarioLogin.get().setSenha(usuario.get().getSenha());
 				usuarioLogin.get().setToken(gerarBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
-
+				usuarioLogin.get().setFoto(usuario.get().getFoto());
+				usuarioLogin.get().setTipo(usuario.get().getTipo());
 				return usuarioLogin;
 
 			}
